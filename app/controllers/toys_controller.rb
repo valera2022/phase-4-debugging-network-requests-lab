b04ec5cade54fa6cfde3aea74928edb54f138059
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-22 10:40:13 -0400

    commit tree 9767d82d6ac58bd8d023cabcb4d51f2f81b0dfd0

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,7 +7,8 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    # byebug
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
@@ -25,7 +26,7 @@ class ToysController < ApplicationController
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:name, :image, :likes,:id)
   end
 
 end
